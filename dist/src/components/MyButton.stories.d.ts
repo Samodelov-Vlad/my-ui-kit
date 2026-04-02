a3df95883d860e31a01875d0359131e422e46322
Vlad <email@example.com>
2026-04-02 15:28:47 +0700

    fix: rebuilt the project

---
+++ dist/src/components/MyButton.stories.d.ts
@@ -0,0 +1,8 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { MyButton } from './MyButton';
+declare const meta: Meta<typeof MyButton>;
+export default meta;
+type Story = StoryObj<typeof MyButton>;
+export declare const Primary: Story;
+export declare const Secondary: Story;
+//# sourceMappingURL=MyButton.stories.d.ts.map
