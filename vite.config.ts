da7fc3d435cad4fce9c35dc1f6b76b3d0ca0c03e
Vlad <email@example.com>
2026-04-02 16:26:52 +0700

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -9,50 +9,32 @@ import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
 import { playwright } from '@vitest/browser-playwright';
 const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
 
-// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react(), dts({
-    insertTypesEntry: true
-  }) // Генерирует файлы определений типов (.d.ts)
-  ],
+  plugins: [react(), dts({ insertTypesEntry: true })],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'MyUiKit',
       fileName: 'index',
-      formats: ['es'] // Указываем только современный формат ES Modules
+      formats: ['es']
     },
     rollupOptions: {
-      // Указываем, какие библиотеки не нужно включать в сборку (они уже есть в проекте-потребителе)
       external: ['react', 'react-dom'],
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM'
-        }
+        },
+        exports: 'named'
       }
+    },
+    // 👇 Добавить это:
+    commonjsOptions: {
+      include: [],  // Не обрабатывать CJS модули
+      dynamicRequireTargets: []
     }
   },
-  test: {
-    projects: [{
-      extends: true,
-      plugins: [
-      // The plugin will run tests for the stories defined in your Storybook config
-      // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
-      storybookTest({
-        configDir: path.join(dirname, '.storybook')
-      })],
-      test: {
-        name: 'storybook',
-        browser: {
-          enabled: true,
-          headless: true,
-          provider: playwright({}),
-          instances: [{
-            browser: 'chromium'
-          }]
-        }
-      }
-    }]
+  optimizeDeps: {
+    exclude: ['react', 'react-dom']  // Исключить из оптимизации
   }
-});
+})
