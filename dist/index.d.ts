846f5f6825f016f7620626952438439ef9b4dfe7
Vlad <email@example.com>
2026-05-18 23:31:22 +0700

    fix d.ts

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
 export * from './components/AntdButton/index';
 export * from './components/AntdInput/index';
+export * from './components/AntdSelect/index';
 //# sourceMappingURL=index.d.ts.map
--- dist/index.js
+++ dist/index.js
@@ -8,6 +8,12 @@ var i = n(({ ...t }, n) => /* @__PURE__ */ r(e, {
 })), a = n(({ ...e }, n) => /* @__PURE__ */ r(t, {
 	ref: n,
 	...e
+})), o = n(({ ...e }, t) => /* @__PURE__ */ r(o, {
+	ref: t,
+	showSearch: !0,
+	optionFilterProp: "label",
+	allowClear: !0,
+	...e
 }));
 //#endregion
-export { i as AntdButton, a as AntdInput };
+export { i as AntdButton, a as AntdInput, o as AntdSelect };
