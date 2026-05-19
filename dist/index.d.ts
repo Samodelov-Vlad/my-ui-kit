847eaf00e56f6d46d54e9d4be661de64b832d2fe
Vlad <email@example.com>
2026-05-19 10:07:39 +0700

    1

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './components/ui/button/index';
-export * from './components/ui/input/index';
-export * from './components/ui/select/index';
-export * from './components/ui/text-area/index';
-export * from './components/ui/date-picker/index';
-//# sourceMappingURL=index.d.ts.map
