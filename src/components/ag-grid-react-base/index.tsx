3a7f7227c38950d358712a72534b7d460276b9e4
Vlad <email@example.com>
2026-05-19 16:12:38 +0700

    1

--- src/components/ag-grid-react-base/index.tsx
+++ src/components/ag-grid-react-base/index.tsx
@@ -3,13 +3,13 @@ import {
   themeQuartz,
   AllCommunityModule,
 } from "ag-grid-community";
-// import { AllEnterpriseModule } from 'ag-grid-enterprise';
+import { AllEnterpriseModule } from "ag-grid-enterprise";
 import { AgGridReact, type AgGridReactProps } from "ag-grid-react";
 import { AG_GRID_LOCALE_RU } from "./ag-grid-locale.ru";
 // Регистрируем модули один раз здесь
 // ModuleRegistry.registerModules([AllEnterpriseModule]);
 
-ModuleRegistry.registerModules([AllCommunityModule]);
+// ModuleRegistry.registerModules([AllCommunityModule]);
 
 // Типизируем пропсы: наследуем все стандартные пропсы AG Grid
 interface AgGridReactBaseProps<TData = any> extends AgGridReactProps<TData> {}
